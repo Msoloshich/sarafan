503d40c6544f77402a413a7cd54cd33921670ec1
Makar Soloshich <msoloshich@gmail.com>
2018-12-22 21:07:58 +0400

    Add oAuth2 authorization (client side)

--- src/main/java/com/example/sarafan/config/WebSecurityConfig.java
+++ src/main/java/com/example/sarafan/config/WebSecurityConfig.java
@@ -10,19 +10,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import javax.validation.constraints.Email;
+
 import java.time.LocalDateTime;
 
 @Configuration
 @EnableWebSecurity
 @EnableOAuth2Sso
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/**")
                 .authorizeRequests()
-                .mvcMatchers("/").permitAll()
+                .antMatchers("/", "/login**", "/js/**", "/error**").permitAll()
                 .anyRequest().authenticated()
+                .and().logout().logoutSuccessUrl("/").permitAll()
                 .and()
                 .csrf().disable();
     }
